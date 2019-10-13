5660d6952cc4805a74ea04401de1aadb82e59f28
Ms2ger <Ms2ger@gmail.com>
2019-10-13 20:10:52 +0200

    Fix typo in CastDate.

--- polyfill/test/date.mjs
+++ polyfill/test/date.mjs
@@ -6,3 +6,6 @@ assert.equal(date.dayOfWeek, 2);
 
 date = new Temporal.Date(2019, 10, 6);
 assert.equal(date.dayOfWeek, 7);
+
+let dateFromDate = Temporal.Date.from(date);
+assert.equal(dateFromDate, date);
