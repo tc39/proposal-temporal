fccdf8899166b4b0c7ab4ea4023a00b115f7d107
Ms2ger <Ms2ger@gmail.com>
2019-10-13 20:13:38 +0200

    Add missing import in casts.mjs.

--- polyfill/test/date.mjs
+++ polyfill/test/date.mjs
@@ -9,3 +9,8 @@ assert.equal(date.dayOfWeek, 7);
 
 let dateFromDate = Temporal.Date.from(date);
 assert.equal(dateFromDate, date);
+
+let datetime = new Temporal.DateTime(2019, 10, 1, 14, 20, 36);
+let dateFromDatetime = Temporal.Date.from(datetime);
+assert.notEqual(dateFromDatetime, datetime);
+assert.equal(dateFromDatetime instanceof Temporal.Date, true);
