df17ee5d57a50acecc4461ca991a4217b46d28a6
Ms2ger <Ms2ger@gmail.com>
2019-10-13 20:19:51 +0200

    Fix typo in Time.fromString.

--- polyfill/test/time.mjs
+++ polyfill/test/time.mjs
@@ -4,3 +4,8 @@ import { strict as assert } from 'assert';
 let datetime = new Temporal.DateTime(2019, 10, 1, 14, 20, 36);
 let time = Temporal.Time.from(datetime);
 assert.notEqual(time, datetime);
+
+time = Temporal.Time.fromString("20:18:32");
+assert.equal(time.hour, 20);
+assert.equal(time.minute, 18);
+assert.equal(time.second, 32);
