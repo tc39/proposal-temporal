5c57d9657149382cfc7c54858d0a106f727f63ee
Philipp Dunkel <pdunkel1@bloomberg.com>
2019-10-28 17:17:04 +0000

    Add duration.toLocaleString

--- polyfill/test/exhaust.mjs
+++ polyfill/test/exhaust.mjs
@@ -11,29 +11,29 @@ let cnt = 0;
 let fail = 0;
 const sts = Temporal.getAbsolute();
 for (let one = start; !!Temporal.Date.compare(one, end); one = one.plus('P1D')) {
-    for (let two = one; !!Temporal.Date.compare(two, end); two = two.plus('P1D')) {
-        const ok = test(one, two);
-        cnt++;
-        if (!(cnt % 100_000)) process.stderr.write('*');
-        if (ok) {
-            if (all) {
-                idx++;
-                console.log(`${ok?'ok':'not ok'} ${idx} < ${one} : ${two} >`);
-            }
-        } else {
-            fail++;
-            idx++;
-            console.log(`${ok?'ok':'not ok'} ${idx} < ${one} : ${two} >`);
-        }
+  for (let two = one; !!Temporal.Date.compare(two, end); two = two.plus('P1D')) {
+    const ok = test(one, two);
+    cnt++;
+    if (!(cnt % 100_000)) process.stderr.write('*');
+    if (ok) {
+      if (all) {
+        idx++;
+        console.log(`${ok ? 'ok' : 'not ok'} ${idx} < ${one} : ${two} >`);
+      }
+    } else {
+      fail++;
+      idx++;
+      console.log(`${ok ? 'ok' : 'not ok'} ${idx} < ${one} : ${two} >`);
     }
+  }
 }
 const ets = Temporal.getAbsolute();
 console.log(`1..${idx}`);
-console.log(`# Sucess: ${cnt-fail}/${cnt} (${ets.difference(sts)})`);
+console.log(`# Sucess: ${cnt - fail}/${cnt} (${ets.difference(sts)})`);
 
 function test(one, two) {
-    const dif = one.difference(two);
-    const plus = `${one.plus(dif)}` === `${two}`;
-    const minus = `${two.minus(dif)}` === `${one}`;
-    return plus && minus;
+  const dif = one.difference(two);
+  const plus = `${one.plus(dif)}` === `${two}`;
+  const minus = `${two.minus(dif)}` === `${one}`;
+  return plus && minus;
 }
