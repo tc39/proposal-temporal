86e2082e73cda096292553168e9e1678e15ea291
Philipp Dunkel <pdunkel1@bloomberg.com>
2019-10-29 10:14:04 +0000

    Make tests signal failure to npm test

--- polyfill/test/datetime.mjs
+++ polyfill/test/datetime.mjs
@@ -244,4 +244,5 @@ describe('DateTime', () => {
 });
 
 import { normalize } from 'path';
-if (normalize(import.meta.url.slice(8)) === normalize(process.argv[1])) report(reporter);
+if (normalize(import.meta.url.slice(8)) === normalize(process.argv[1]))
+  report(reporter).then((failed) => process.exit(failed ? 1 : 0));
