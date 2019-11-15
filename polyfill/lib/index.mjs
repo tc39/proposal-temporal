90a82554650121fd1d1df31eb65706a03961fbcd
Ms2ger <Ms2ger@gmail.com>
2019-11-15 17:35:04 +0100

    Fix bugs in setup().

--- polyfill/lib/index.mjs
+++ polyfill/lib/index.mjs
@@ -1,5 +1,24 @@
 import * as Temporal from './temporal.mjs';
 import * as Intl from './intl.mjs';
 
-globalThis.Temporal = Temporal;
+Object.defineProperty(globalThis, "Temporal", {
+  value: {},
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
+copy(globalThis.Temporal, Temporal);
+copy(globalThis.Intl, Intl);
+
+function copy(target, source) {
+  for (const prop of Object.getOwnPropertyNames(source)) {
+    Object.defineProperty(target, prop, {
+      value: source[prop],
+      writable: true,
+      enumerable: false,
+      configurable: true,
+    });
+  }
+}
+
 export { Temporal, Intl };
