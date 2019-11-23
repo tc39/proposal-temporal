68a0b624d2f0aa74fc48a179d287a15cdaf7d6a1
Philipp Dunkel <pipobscure@users.noreply.github.com>
2019-11-23 11:01:32 +0000

    Regex Update to parse ISO-Strings with ' ' instead of T separating date & time components (#229)
    Merging this for progress

--- polyfill/test/all.mjs
+++ polyfill/test/all.mjs
@@ -9,6 +9,7 @@ import Demitasse from '@pipobscure/demitasse';
 import Pretty from '@pipobscure/demitasse-pretty';
 
 import * as exports from './exports.mjs';
+// import * as regex from './regex.mjs';
 
 import * as timezone from './timezone.mjs';
 import * as absolute from './absolute.mjs';
@@ -16,10 +17,11 @@ import * as date from './date.mjs';
 import * as time from './time.mjs';
 import * as datetime from './datetime.mjs';
 import * as duration from './duration.mjs';
-import * as datemath from './datemath.mjs';
 import * as yearmonth from './yearmonth.mjs';
 import * as monthday from './monthday.mjs';
 import * as intl from './intl.mjs';
+
+import * as datemath from './datemath.mjs';
 
 Promise.resolve()
   .then(() => {
