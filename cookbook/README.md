0c0ce75bdc51ab18ab1e4f0145a47e987bada00f
Jason Williams <jwilliams720@bloomberg.net>
2020-01-03 11:45:41 +0000

    Adding parsableZonedString

--- cookbook/README.md
+++ cookbook/README.md
@@ -2,6 +2,10 @@
 
 ## Construction
 
-[Absolute from legacy Date](./absoluteFromLegacyDate.mjs)
+### [Absolute from legacy Date](./absoluteFromLegacyDate.mjs)
 
 Map a legacy ECMAScript Date instance into a Temporal.Absolute instance corresponding to the same instant in absolute time.
+
+### [Zoned instant from instant and time zone](./getParseableZonedStringAtInstant.mjs)
+
+Map a Temporal.Absolute instance and a time zone name into a string serialization of the local time in that zone corresponding to the instant in absolute time.
---
+++ cookbook/getParseableZonedStringAtInstant.mjs
@@ -0,0 +1,18 @@
+import { Temporal } from "../polyfill/lib/index.mjs";
+
+/**
+ * Converts an absolute point in time to a readable string of a different time zone
+ *
+ * @param {Temporal.Absolute} absolute An absolute point in time
+ * @returns {string} IANA Timezone
+ */
+function getParseableZonedStringAtInstant(absolute, ianaTimeZoneName) {
+    // inTimeZone accepts either a timezone string or a Temporal.TimeZone object
+    // It returns a DateTime object
+    const localDateTime = absolute.inTimeZone(ianaTimeZoneName);
+    return localDateTime.toString();
+}
+
+const absoluteTime = Temporal.Absolute.from("2020-01-03T10:41:51Z");
+const timeZone = "Europe/Paris";
+getParseableZonedStringAtInstant(absoluteTime, timeZone); // '2020-01-03T11:41:51'
