197414a124e9c1a48987047e3ba1dc3b9f046c5f
Philip Chimento <pchimento@igalia.com>
2020-01-23 15:27:58 -0800

    Bring UTC offset string in line with ISO 8601
    When accepting a bare UTC offset string as a time zone name, we want to
    accept any string that would be a valid UTC offset in ISO 8601 (and
    reject any string that wouldn't.)

--- polyfill/lib/regex.mjs
+++ polyfill/lib/regex.mjs
@@ -1,11 +1,11 @@
 const yearpart = /(?:[+-]\d{6}|\d{4})/;
 const datepart = new RegExp(`(?:${yearpart.source}-\\d{2}-\\d{2})`);
 const timepart = /(?:\d{2}\:\d{2}(?:\:\d{2}(?:\.\d{3}(?:\d{3}(?:\d{3})?)?)?)?)/;
-const zonepart = /(?:Z|(?:[+-]\d{1,2}\:?\d{2}(?:\[[^\]\s]+\])?))/;
+const zonepart = /(?:Z|(?:[+-]\d{2}(?:\:?\d{2})?(?:\[[^\]\s]+\])?))/;
 
 const datesplit = new RegExp(`(${yearpart.source})-(\\d{2})-(\\d{2})`);
 const timesplit = /(\d{2})\:(\d{2})(?:\:(\d{2})(?:\.(\d{3})(\d{3})?(\d{3})?)?)?/;
-const zonesplit = /(?:(Z)|(?:([+-]\d{1,2}\:?\d{2})(?:\[([^\]\s]+)\])?))/;
+const zonesplit = /(?:(Z)|(?:([+-]\d{2})(?:\:?(\d{2}))?(?:\[([^\]\s]+)\])?))/;
 
 export const absolute = new RegExp(`^${datesplit.source}(?:T|\\s+)${timesplit.source}${zonesplit.source}$`);
 export const datetime = new RegExp(`^${datesplit.source}(?:T|\\s+)${timesplit.source}(?:${zonepart.source})?$`);
@@ -17,13 +17,13 @@ export const yearmonth = new RegExp(
 );
 
 export const monthday = new RegExp(
-  '^' +
+  '^(?:' +
     [
       new RegExp(`(?:${yearpart.source}-(\\d{2})-(\\d{2})(?:(?:T|\\s+)${timepart.source}${zonepart.source}?)?)`).source,
       new RegExp(`(?:(\\d{2})-(\\d{2}))`).source
     ].join('|') +
-    '$'
+    ')$'
 );
 
-export const offset = /([+-])([01]?[0-9]):?([0-5][0-9])/;
+export const offset = /([+-])([0-2][0-9])(?::?([0-5][0-9]))?/;
 export const duration = /P(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d+)?)S)?)?/;
