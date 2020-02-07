017b148aae60dee5814ee92f5de0ad36f62d9438
Jason Williams <jwilliams720@bloomberg.net>
2020-02-07 17:29:18 +0000

    updating polyfill

--- docs/cookbook/sortAbsoluteInstants.mjs
+++ docs/cookbook/sortAbsoluteInstants.mjs
@@ -1,28 +1,33 @@
+import assert from "assert";
+
 /**
  * getSortedInstants will sort an array of strings (each of which is parseable as a Temporal.Absolute and may or may not include an IANA time zone name)
  * by the corresponding absolute time (e.g., for presenting global log events sequentially).
  *
  *
- * @param {string[]} parseableAbsoluteStrings This is a Date instance
- * @param {boolean} reverse accending or descending order
+ * @param {string[]} parseableAbsoluteStrings - a group of ISO Strings
+ * @param {boolean} reverse ascending or descending order
  * @returns {Temporal.Absolute[]} Temporal.Absolute instance
  */
 function getSortedInstants(parseableAbsoluteStrings, reverse = false) {
-    const absoluteTimes = parseableAbsoluteStrings.map(v => {
-        return Temporal.Absolute.from(v);
-    });
-
-    const sortedAbsoluteTimes = absoluteTimes.sort((a, b) => {
-        return a.compare(b);
-    });
+    const sortedAbsoluteTimes = parseableAbsoluteStrings.sort(
+        Temporal.Absolute.compare
+    );
 
     return reverse ? sortedAbsoluteTimes.reverse() : sortedAbsoluteTimes;
 }
 
-let a = "2020-01-23T17:04:36.491865121-08:00";
-let b = "2020-02-10T17:04:36.491865121-08:00";
-let c = "2019-03-30T01:45:00+01:00[Europe/Berlin]";
-let d = "2019-03-30T01:45:00+00:00[Europe/London]";
-let e = "2019-03-30T01:45:00-06:00[America/New_York]";
+const a = "2020-01-23T17:04:36.491865121-08:00";
+const b = "2020-02-10T17:04:36.491865121-08:00";
+const c = "2019-03-30T01:45:00+01:00[Europe/Berlin]";
+const d = "2019-03-16T01:45:00+00:00[Europe/London]";
+const e = "2019-03-25T01:45:00-06:00[America/New_York]";
 
-getSortedInstants([a, b, c, d, e]);
+const results = getSortedInstants([a, b, c, d, e]);
+
+// results will have correct order
+assert.equal(results[0], d);
+assert.equal(results[1], e);
+assert.equal(results[2], c);
+assert.equal(results[3], a);
+assert.equal(results[4], b);
