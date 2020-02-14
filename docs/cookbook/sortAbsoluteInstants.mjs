69ee22a507831caab6a69b93a808989b9d0924f1
Jason Williams <jwilliams720@bloomberg.net>
2020-02-14 17:45:28 +0000

    decoating input values

--- docs/cookbook/sortAbsoluteInstants.mjs
+++ docs/cookbook/sortAbsoluteInstants.mjs
@@ -7,13 +7,13 @@ import assert from "assert";
  *
  * @param {string[]} parseableAbsoluteStrings - a group of ISO Strings
  * @param {boolean} reverse ascending or descending order
- * @returns {Temporal.Absolute[]} Temporal.Absolute instance
+ * @returns {string[]} Temporal.Absolute instance
  */
 function getSortedInstants(parseableAbsoluteStrings, reverse = false) {
-    const absoluteObjects = parseableAbsoluteStrings.map(v => {
-        return Temporal.Absolute.from(v);
-    });
-    const sortedAbsoluteTimes = absoluteObjects.sort(Temporal.Absolute.compare);
+    const sortedAbsoluteTimes = parseableAbsoluteStrings
+        .map(v => [v, Temporal.Absolute.from(v)])
+        .sort(([, abs1], [, abs2]) => Temporal.Absolute.compare(abs1, abs2))
+        .map(([str]) => str);
 
     return reverse ? sortedAbsoluteTimes.reverse() : sortedAbsoluteTimes;
 }
@@ -27,8 +27,8 @@ const e = "2019-03-25T01:45:00-06:00[America/New_York]";
 const results = getSortedInstants([a, b, c, d, e]);
 
 // results will have correct order
-assert.equal(results[0].toString(), "2019-03-16T01:45Z");
-assert.equal(results[1].toString(), "2019-03-25T05:45Z");
-assert.equal(results[2].toString(), "2019-03-30T00:45Z");
-assert.equal(results[3].toString(), "2020-01-24T01:04:36.491865121Z");
-assert.equal(results[4].toString(), "2020-02-11T01:04:36.491865121Z");
+assert.equal(results[0], "2019-03-16T01:45:00+00:00[Europe/London]");
+assert.equal(results[1], "2019-03-25T01:45:00-06:00[America/New_York]");
+assert.equal(results[2], "2019-03-30T01:45:00+01:00[Europe/Berlin]");
+assert.equal(results[3], "2020-01-23T17:04:36.491865121-08:00");
+assert.equal(results[4], "2020-02-10T17:04:36.491865121-08:00");
