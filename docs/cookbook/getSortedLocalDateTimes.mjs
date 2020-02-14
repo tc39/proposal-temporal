ea0131a55831f7196af1569d2f7fa5fff7b270cf
Jason Williams <jwilliams720@bloomberg.net>
2020-02-14 18:01:13 +0000

    updating using from instead of new and proper compare

--- docs/cookbook/getSortedLocalDateTimes.mjs
+++ docs/cookbook/getSortedLocalDateTimes.mjs
@@ -4,18 +4,16 @@
  *
  *
  * @param {Temporal.DateTime[]} dateTimes This is a Date instance
- * @param {boolean} direction accending or descending order
- * @returns {Temporal.Absolute} Temporal.Absolute instance
+ * @param {boolean} direction ascending or descending order
+ * @returns {Temporal.DateTime[]} Temporal.Absolute instance
  */
 function getSortedLocalDateTimes(dateTimes, reverse = false) {
-    let newDateTimes = dateTimes.sort((a, b) => {
-        return a.compare(b);
-    });
+    let newDateTimes = dateTimes.sort(Temporal.DateTime.compare);
 
     return reverse ? newDateTimes.reverse() : newDateTimes;
 }
 
-let a = new Temporal.DateTime({ day: 4, month: 2 });
-let b = new Temporal.DateTime({ day: 5, month: 2 });
-let c = new Temporal.DateTime({ day: 6, month: 3 });
+let a = Temporal.DateTime.from({ day: 4, month: 2 });
+let b = Temporal.DateTime.from({ day: 5, month: 2 });
+let c = Temporal.DateTime.from({ day: 6, month: 3 });
 getSortedLocalDateTimes([a, b, c]);
