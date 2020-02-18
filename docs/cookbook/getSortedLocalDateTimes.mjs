6f3e60fcd080882f128bf916afe5b99f85a6bdce
Jason Williams <jwilliams720@bloomberg.net>
2020-02-18 14:59:19 +0000

    * using Array.from * using same conference

--- docs/cookbook/getSortedLocalDateTimes.mjs
+++ docs/cookbook/getSortedLocalDateTimes.mjs
@@ -4,20 +4,38 @@ import assert from "assert";
  * corresponding local date and time of day (e.g., for building a conference schedule).
  *
  *
- * @param {Temporal.DateTime[]} dateTimes This is a Date instance
- * @param {boolean} direction ascending or descending order
- * @returns {Temporal.DateTime[]} Temporal.Absolute instance
+ * @param {Temporal.DateTime[]} dateTimes This is a DateTime instance
+ * @param {boolean} direction reverse
+ * @returns {Temporal.DateTime[]} the array from dateTimes, sorted
  */
 function getSortedLocalDateTimes(dateTimes, reverse = false) {
-    let newDateTimes = dateTimes.sort(Temporal.DateTime.compare);
+    let newDateTimes = Array.from(dateTimes).sort(Temporal.DateTime.compare);
 
     return reverse ? newDateTimes.reverse() : newDateTimes;
 }
 
-// Sorting some conferences without timezones
-let a = Temporal.DateTime.from({ year: 2020, day: 15, month: 2, hour: 9 }); // FontFest
-let b = Temporal.DateTime.from({ year: 2020, day: 26, month: 5 }); // React Finland
-let c = Temporal.DateTime.from({ year: 2020, day: 23, month: 2, hour: 9 }); // ReactConf AU
+// Sorting some conferences without timezones for example vue.js Amsterdam 2020
+let a = Temporal.DateTime.from({
+    year: 2020,
+    day: 20,
+    month: 2,
+    hour: 8,
+    minute: 45
+}); // Introduction
+let b = Temporal.DateTime.from({
+    year: 2020,
+    day: 21,
+    month: 2,
+    hour: 13,
+    minute: 10
+}); // Lunch Break
+let c = Temporal.DateTime.from({
+    year: 2020,
+    day: 20,
+    month: 2,
+    hour: 15,
+    minute: 30
+}); // Coffee Break
 const results = getSortedLocalDateTimes([a, b, c]);
 assert.equal(results[0], a);
 assert.equal(results[1], c);
