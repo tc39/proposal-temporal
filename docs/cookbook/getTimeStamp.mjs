f41f50b46bd62f6f745fdc9b226cf7b5c0271603
Jason Williams <jwilliams720@bloomberg.net>
2020-02-28 14:21:38 +0000

    get timestamp in JS fixes #240

---
+++ docs/cookbook/getTimeStamp.mjs
@@ -0,0 +1,15 @@
+import assert from "assert";
+
+/**
+ * Get a (Unix) timestamp in JavaScript
+ * This is the No.1 voted question on Stack Overflow for dates in JS
+ * https://stackoverflow.com/questions/221294/how-do-you-get-a-timestamp-in-javascript
+ *
+ */
+
+const timeStamp = Temporal.now.absolute();
+
+// Timestamp in Milliseconds
+timeStamp.getEpochMilliseconds();
+// Timestamp in Seconds
+timeStamp.getEpochSeconds();
