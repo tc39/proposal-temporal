f457a4162611c42ab4c01416d54f8eb2d2290632
Ms2ger <Ms2ger@gmail.com>
2020-03-23 15:21:02 +0100

    Clarify the SystemTimeZone() abstract operation.

---
+++ polyfill/test/now/timeZone/new-object.js
@@ -0,0 +1,11 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.now.timezone
+---*/
+
+const tz = Temporal.now.timeZone;
+const tz1 = tz();
+const tz2 = tz();
+assert.notSameValue(tz1, tz2);
