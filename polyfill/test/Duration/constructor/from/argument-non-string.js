d8ed64afc85654abcadb7fdab7a28d103eccab4d
Ms2ger <Ms2ger@gmail.com>
2020-03-25 15:39:31 +0100

    Align the specification for Duration.from with the polyfill.

---
+++ polyfill/test/Duration/constructor/from/argument-non-string.js
@@ -0,0 +1,13 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-get-temporal.duration.from
+---*/
+
+assert.throws(RangeError, () => Temporal.Duration.from(undefined), "undefined");
+assert.throws(RangeError, () => Temporal.Duration.from(null), "null");
+assert.throws(RangeError, () => Temporal.Duration.from(true), "boolean");
+assert.throws(TypeError, () => Temporal.Duration.from(Symbol()), "Symbol");
+assert.throws(RangeError, () => Temporal.Duration.from(5), "number");
+assert.throws(RangeError, () => Temporal.Duration.from(5n), "bigint");
