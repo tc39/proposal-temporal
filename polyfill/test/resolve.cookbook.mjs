22f3b5d9222d7ab12729e196e88cef8fc93d9282
Philip Chimento <pchimento@igalia.com>
2020-04-07 13:56:05 -0700

    Polyfill the Intl object in the cookbook examples as well
    In order for toLocaleString() to work in the cookbook, we'll need to
    install our Intl polyfill on the global Intl object. This requires a
    small change in the cookbook resolver.

--- polyfill/test/resolve.cookbook.mjs
+++ polyfill/test/resolve.cookbook.mjs
@@ -7,15 +7,15 @@ import fs from 'fs';
 const PKG = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
 export function resolve(specifier, parent, defaultResolve) {
   if (specifier === PKG.name) {
-    specifier = new URL('../lib/temporal.mjs', import.meta.url).toString();
+    specifier = new URL('../lib/index.mjs', import.meta.url).toString();
   }
   return defaultResolve(specifier, parent);
 }
 
 export async function transformSource(source, { url, format }, defaultTransformSource) {
-  if (typeof source === 'string' && url !== 'all.mjs') {
+  if (typeof source === 'string' && url !== 'all.mjs' && !url.endsWith('polyfill/lib/index.mjs')) {
     return {
-      source: "import * as Temporal from 'tc39-temporal';\n" + source
+      source: "import { Temporal } from 'tc39-temporal';\n" + source
     };
   } else {
     // source could be a buffer, e.g. for WASM
