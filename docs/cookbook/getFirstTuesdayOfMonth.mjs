e62ebb8bd471402b7df0be279a4893bf442c2f47
Philip Chimento <pchimento@igalia.com>
2020-04-08 17:25:15 -0700

    Add more Nth-day-of-the-week examples
    Augmenting the existing getFirstTuesday example, there were a few more
    that Richard and Shane mentioned in the issue
    
    See: #240

--- docs/cookbook/getFirstTuesdayOfMonth.mjs
+++ docs/cookbook/getFirstTuesdayOfMonth.mjs
@@ -22,3 +22,17 @@ const myMonth = Temporal.YearMonth.from('2020-02');
 const firstTuesdayOfMonth = getFirstTuesday(myMonth);
 assert.equal(firstTuesdayOfMonth.toString(), '2020-02-04');
 assert.equal(firstTuesdayOfMonth.dayOfWeek, 2);
+
+// Similarly, to get the second Tuesday:
+const secondWeek = myMonth.withDay(8);
+const secondTuesday = secondWeek.plus({ days: [1, 0, 6, 5, 4, 3, 2][secondWeek.dayOfWeek - 1] });
+assert.equal(secondTuesday.day, firstTuesdayOfMonth.day + 7);
+
+// And the last Tuesday:
+const lastOfMonth = myMonth.withDay(myMonth.daysInMonth);
+const lastTuesday = lastOfMonth.minus({ days: [6, 0, 1, 2, 3, 4, 5][lastOfMonth.dayOfWeek - 1] });
+assert.equal(lastTuesday.toString(), '2020-02-25');
+assert.equal(lastTuesday.dayOfWeek, 2);
+// or:
+const lastTuesday2 = lastOfMonth.minus({ days: (7 + lastOfMonth.dayOfWeek - 2) % 7 });
+assert.equal(Temporal.Date.compare(lastTuesday, lastTuesday2), 0);
