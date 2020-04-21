1ce03e1de19662d792ec23772d5f46b671681800
Philip Chimento <pchimento@igalia.com>
2020-04-21 02:25:59 -0700

    Add airline examples to cookbook (#514)
    See: #240

---
+++ docs/cookbook/getLocalizedArrival.mjs
@@ -0,0 +1,22 @@
+/**
+ * Given a localized departure time and a flight duration, get a local arrival
+ * time in the destination time zone.
+ *
+ * @param {string} parseableDeparture - Departure time with time zone
+ * @param {Temporal.Duration} flightTime - Duration of the flight
+ * @param {Temporal.TimeZone} destinationTimeZone - Time zone in which the
+ *  flight's destination is located
+ * @returns {Temporal.DateTime} Local arrival time
+ */
+function getLocalizedArrival(parseableDeparture, flightTime, destinationTimeZone) {
+  const departure = Temporal.Absolute.from(parseableDeparture);
+  const arrival = departure.plus(flightTime);
+  return arrival.inTimeZone(destinationTimeZone);
+}
+
+const arrival = getLocalizedArrival(
+  '2020-03-08T11:55:00+08:00[Asia/Hong_Kong]',
+  Temporal.Duration.from({ minutes: 775 }),
+  'America/Los_Angeles'
+);
+assert.equal(`${arrival}`, '2020-03-08T09:50');
---
+++ docs/cookbook/getTripDurationInHrMinSec.mjs
@@ -0,0 +1,19 @@
+/**
+ * Given localized departure and arrival times, get a trip duration suitable
+ * for display in an airline ticket website, for example.
+ *
+ * @param {string} parseableDeparture - Departure time with time zone
+ * @param {string} parseableArrival - Arrival time with time zone
+ * @returns {Temporal.Duration} A duration with units no larger than hours
+ */
+function getTripDurationInHrMinSec(parseableDeparture, parseableArrival) {
+  const departure = Temporal.Absolute.from(parseableDeparture);
+  const arrival = Temporal.Absolute.from(parseableArrival);
+  return departure.difference(arrival, { largestUnit: 'hours' });
+}
+
+const flightTime = getTripDurationInHrMinSec(
+  '2020-03-08T11:55:00+08:00[Asia/Hong_Kong]',
+  '2020-03-08T09:50:00-07:00[America/Los_Angeles]'
+);
+assert.equal(`${flightTime}`, 'PT12H55M');
