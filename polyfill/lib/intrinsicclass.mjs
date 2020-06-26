06e85cb1828cfc1d976c4a7a877d45c880213470
Philip Chimento <pchimento@igalia.com>
2020-06-26 15:48:42 -0700

    Define Node.js custom util.inspect formatters in debug mode
    If __debug__ is defined to true, Temporal objects will gain a custom
    util.inspect formatter. Like the Chrome devtools formatter, it will
    print the class name and the toString() output between angle brackets,
    and for Temporal.Duration it will print the nonzero fields.
    
    This should not increase the bundle size because Rollup will eliminate
    the dead code if __debug__ is defined to false at build time.
    
    See: #643

--- polyfill/lib/intrinsicclass.mjs
+++ polyfill/lib/intrinsicclass.mjs
@@ -1,6 +1,35 @@
+/* global __debug__ */
+
 import ESGetIntrinsic from 'es-abstract/GetIntrinsic.js';
 
 const INTRINSICS = {};
+
+const customUtilInspectFormatters = {
+  ['Temporal.Duration'](depth, options) {
+    const descr = options.stylize(`${this[Symbol.toStringTag]} <${this}>`, 'special');
+    if (depth < 1) return descr;
+    const entries = [];
+    for (const prop of [
+      'years',
+      'months',
+      'weeks',
+      'days',
+      'hours',
+      'minutes',
+      'seconds',
+      'milliseconds',
+      'microseconds',
+      'nanoseconds'
+    ]) {
+      if (this[prop] !== 0) entries.push(`  ${prop}: ${options.stylize(this[prop], 'number')}`);
+    }
+    return descr + ' {\n' + entries.join(',\n') + '\n}';
+  }
+};
+
+function defaultUtilInspectFormatter(depth, options) {
+  return options.stylize(`${this[Symbol.toStringTag]} <${this}>`, 'special');
+}
 
 export function MakeIntrinsicClass(Class, name) {
   Object.defineProperty(Class.prototype, Symbol.toStringTag, {
@@ -9,6 +38,14 @@ export function MakeIntrinsicClass(Class, name) {
     enumerable: false,
     configurable: true
   });
+  if (typeof __debug__ !== 'undefined' && __debug__) {
+    Object.defineProperty(Class.prototype, Symbol.for('nodejs.util.inspect.custom'), {
+      value: customUtilInspectFormatters[name] || defaultUtilInspectFormatter,
+      writable: false,
+      enumerable: false,
+      configurable: true
+    });
+  }
   const species = function () {
     return this;
   };
