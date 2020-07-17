ba1b39aaf435c93ed953b3a9993c8e825a2f32f1
Ms2ger <Ms2ger@gmail.com>
2020-07-17 11:52:16 +0200

    Formalize details for TimeZone.name.

---
+++ polyfill/test/TimeZone/prototype/name/plain-custom-timezone.js
@@ -0,0 +1,35 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.timezone.prototype.name
+includes: [compareArray.js]
+---*/
+
+const actual = [];
+const expected = [
+  "get timeZone.toString",
+  "call timeZone.toString",
+];
+
+const timeZone = new Proxy({
+  toString() {
+    actual.push("call timeZone.toString");
+    return "time zone";
+  },
+}, {
+  has(target, property) {
+    actual.push(`has timeZone.${property}`);
+    return property in target;
+  },
+  get(target, property) {
+    actual.push(`get timeZone.${property}`);
+    return target[property];
+  },
+});
+
+const descriptor = Object.getOwnPropertyDescriptor(Temporal.TimeZone.prototype, "name");
+const result = descriptor.get.call(timeZone);
+assert.sameValue(result, "time zone");
+
+assert.compareArray(actual, expected);
