219a2d9b805803e7cda88aba28184aa708d8177a
Ms2ger <Ms2ger@gmail.com>
2020-08-12 11:18:30 +0200

    Convert calendar argument in default TimeZone.getDateTimeFor.

---
+++ polyfill/test/TimeZone/prototype/getDateTimeFor/calendar-undefined.js
@@ -0,0 +1,21 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.timezone.prototype.getdatetimefor
+---*/
+
+const absolute = Temporal.Absolute.from("1975-02-02T14:25:36.123456789Z");
+const timeZone = Temporal.TimeZone.from("UTC");
+
+Object.defineProperty(Temporal.Calendar, "from", {
+  get() {
+    throw new Test262Error("Should not call Calendar.from");
+  },
+});
+
+const result1 = timeZone.getDateTimeFor(absolute);
+assert.sameValue(result1.calendar.toString(), "iso8601");
+
+const result2 = timeZone.getDateTimeFor(absolute, undefined);
+assert.sameValue(result2.calendar.toString(), "iso8601");
