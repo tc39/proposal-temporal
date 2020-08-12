d92ea08a7a943fcbab67d9454bafbb7b590b9c86
Ms2ger <Ms2ger@gmail.com>
2020-08-12 11:17:00 +0200

    Pass calendars to GetTemporalDateTimeFor in the specification.

---
+++ polyfill/test/now/date/calendar-object.js
@@ -0,0 +1,47 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.now.date
+includes: [compareArray.js]
+---*/
+
+const actual = [];
+const expected = [
+  "get timeZone.getDateTimeFor",
+  "call timeZone.getDateTimeFor",
+];
+const dateTime = Temporal.DateTime.from("1963-07-02T12:34:56.987654321");
+const calendar = {};
+const timeZone = new Proxy({
+  getDateTimeFor(absolute, calendarArg) {
+    actual.push("call timeZone.getDateTimeFor");
+    assert.sameValue(absolute instanceof Temporal.Absolute, true, "Absolute");
+    assert.sameValue(calendarArg, calendar);
+    return dateTime;
+  },
+}, {
+  has(target, property) {
+    actual.push(`has timeZone.${property}`);
+    return property in target;
+  },
+  get(target, property) {
+    actual.push(`get timeZone.${property}`);
+    return target[property];
+  },
+});
+
+Object.defineProperty(Temporal.Calendar, "from", {
+  get() {
+    actual.push("get Temporal.Calendar.from");
+    return undefined;
+  },
+});
+
+const result = Temporal.now.date(timeZone, calendar);
+assert.sameValue(result instanceof Temporal.Date, true);
+for (const property of ["year", "month", "day"]) {
+  assert.sameValue(result[property], dateTime[property], property);
+}
+
+assert.compareArray(actual, expected);
---
+++ polyfill/test/now/dateTime/calendar-object.js
@@ -0,0 +1,44 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.now.datetime
+includes: [compareArray.js]
+---*/
+
+const actual = [];
+const expected = [
+  "get timeZone.getDateTimeFor",
+  "call timeZone.getDateTimeFor",
+];
+const dateTime = Temporal.DateTime.from("1963-07-02T12:34:56.987654321");
+const calendar = {};
+const timeZone = new Proxy({
+  getDateTimeFor(absolute, calendarArg) {
+    actual.push("call timeZone.getDateTimeFor");
+    assert.sameValue(absolute instanceof Temporal.Absolute, true, "Absolute");
+    assert.sameValue(calendarArg, calendar);
+    return dateTime;
+  },
+}, {
+  has(target, property) {
+    actual.push(`has timeZone.${property}`);
+    return property in target;
+  },
+  get(target, property) {
+    actual.push(`get timeZone.${property}`);
+    return target[property];
+  },
+});
+
+Object.defineProperty(Temporal.Calendar, "from", {
+  get() {
+    actual.push("get Temporal.Calendar.from");
+    return undefined;
+  },
+});
+
+const result = Temporal.now.dateTime(timeZone, calendar);
+assert.sameValue(result, dateTime);
+
+assert.compareArray(actual, expected);
