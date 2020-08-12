2299accd86a7d4d89fbb01459a50c15c0f18a031
Ms2ger <Ms2ger@gmail.com>
2020-08-12 11:17:00 +0200

    Improve TimeZone handling in Temporal.now.dateTime().

---
+++ polyfill/test/now/time/timezone-object.js
@@ -0,0 +1,46 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.now.time
+includes: [compareArray.js]
+---*/
+
+const actual = [];
+const expected = [
+  "get timeZone.getDateTimeFor",
+  "call timeZone.getDateTimeFor",
+];
+const dateTime = Temporal.DateTime.from("1963-07-02T12:34:56.987654321");
+
+const timeZone = new Proxy({
+  getDateTimeFor(absolute) {
+    actual.push("call timeZone.getDateTimeFor");
+    assert.sameValue(absolute instanceof Temporal.Absolute, true, "Absolute");
+    return dateTime;
+  },
+}, {
+  has(target, property) {
+    actual.push(`has timeZone.${property}`);
+    return property in target;
+  },
+  get(target, property) {
+    actual.push(`get timeZone.${property}`);
+    return target[property];
+  },
+});
+
+Object.defineProperty(Temporal.TimeZone, "from", {
+  get() {
+    actual.push("get Temporal.TimeZone.from");
+    return undefined;
+  },
+});
+
+const result = Temporal.now.time(timeZone);
+assert.sameValue(result instanceof Temporal.Time, true);
+for (const property of ["hour", "minute", "second", "millisecond", "microsecond", "nanosecond"]) {
+  assert.sameValue(result[property], dateTime[property], property);
+}
+
+assert.compareArray(actual, expected);
