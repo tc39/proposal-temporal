f61cc2cbbda13b2a40347bdc60055a51478ad78b
Ms2ger <Ms2ger@gmail.com>
2020-08-13 16:16:17 +0200

    Verify that getDateTimeFor() returns a DateTime.

---
+++ polyfill/test/now/time/timezone-invalid-result.js
@@ -0,0 +1,30 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.now.time
+---*/
+
+const invalidValues = [
+  undefined,
+  null,
+  true,
+  "2020-01-01T12:45:36",
+  Symbol(),
+  2020,
+  2n,
+  {},
+  Temporal.DateTime,
+  Temporal.DateTime.prototype,
+];
+
+for (const dateTime of invalidValues) {
+  const timeZone = {
+    getDateTimeFor(absolute) {
+      assert.sameValue(absolute instanceof Temporal.Absolute, true, "Absolute");
+      return dateTime;
+    },
+  };
+
+  assert.throws(TypeError, () => Temporal.now.time(timeZone));
+}
