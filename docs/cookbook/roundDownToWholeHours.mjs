a327a77717e58527d41d2ae819508ea96339feaf
Philip Chimento <pchimento@igalia.com>
2020-09-01 12:13:46 -0700

    Update cookbook with new rounding recipes
    Some of the recipes get easier with the round() method. Since we do not
    round date fields, also adds a new recipe with an example of what you
    might do if you want a certain type of date rounding.
    
    See: #827

--- docs/cookbook/roundDownToWholeHours.mjs
+++ docs/cookbook/roundDownToWholeHours.mjs
@@ -1,9 +1,5 @@
 const time = Temporal.Time.from('12:38:28.138818731');
 
-// explicitly:
-let wholeHour = time.with({ minute: 0, second: 0, millisecond: 0, microsecond: 0, nanosecond: 0 });
-assert.equal(wholeHour.toString(), '12:00');
+const wholeHour = time.round({ smallestUnit: 'hour', roundingMode: 'floor' });
 
-// or, taking advantage of 0 being the default for time fields:
-wholeHour = Temporal.Time.from({ hour: time.hour });
 assert.equal(wholeHour.toString(), '12:00');
