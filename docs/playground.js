d65d17def64513a2b14550c51cc25f1488c385b6
Philipp Dunkel <deploy@travis-ci.org>
2020-09-21 16:18:29 +0000

    Deploy tc39/proposal-temporal to github.com/tc39/proposal-temporal.git:gh-pages

--- docs/playground.js
+++ docs/playground.js
@@ -350,11 +350,11 @@
 
   function createCommonjsModule(fn, basedir, module) {
   	return module = {
-  	  path: basedir,
-  	  exports: {},
-  	  require: function (path, base) {
-        return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
-      }
+  		path: basedir,
+  		exports: {},
+  		require: function (path, base) {
+  			return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+  		}
   	}, fn(module, module.exports), module.exports;
   }
 
