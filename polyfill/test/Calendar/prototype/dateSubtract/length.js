7965e5070395f93c14502c631e62ebf24515906f
Philip Chimento <pchimento@igalia.com>
2020-10-09 15:59:04 -0700

    Rename Calendar.datePlus() and Calendar.dateMinus()
    Rename these to dateAdd() and dateSubtract(). I picked these over the
    arguably more natural addDate() and subtractDate() because I thought those
    might be confusing; we are adding a Duration _to_ a Date, not a Date to
    something else.
    
    See: #770

--- polyfill/test/Calendar/prototype/dateSubtract/length.js
+++ polyfill/test/Calendar/prototype/dateSubtract/length.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.calendar.prototype.dateminus
+esid: sec-temporal.calendar.prototype.datesubtract
 info: |
     Every built-in function object, including constructors, has a "length" property whose value is
     an integer. Unless otherwise specified, this value is equal to the largest number of named
@@ -15,7 +15,7 @@ info: |
 includes: [propertyHelper.js]
 ---*/
 
-verifyProperty(Temporal.Calendar.prototype.dateMinus, "length", {
+verifyProperty(Temporal.Calendar.prototype.dateSubtract, "length", {
   value: 4,
   writable: false,
   enumerable: false,
