c976eae647c6d0ceca5d5e9a3306d5076ef0bd8e
Philip Chimento <pchimento@igalia.com>
2020-10-14 14:06:23 -0700

    Add Temporal.now.dateTimeISO() and Temporal.now.dateISO()
    Convenience methods that give the current date and datetime in the ISO
    8601 calendar.
    
    See: #292

--- polyfill/test/now.mjs
+++ polyfill/test/now.mjs
@@ -19,15 +19,24 @@ import * as Temporal from 'proposal-temporal';
 describe('Temporal.now', () => {
   describe('Structure', () => {
     it('Temporal.now is an object', () => equal(typeof Temporal.now, 'object'));
-    it('Temporal.now has 5 properties', () => equal(Object.keys(Temporal.now).length, 5));
+    it('Temporal.now has 7 properties', () => equal(Object.keys(Temporal.now).length, 7));
     it('Temporal.now.instant is a function', () => equal(typeof Temporal.now.instant, 'function'));
     it('Temporal.now.dateTime is a function', () => equal(typeof Temporal.now.dateTime, 'function'));
+    it('Temporal.now.dateTimeISO is a function', () => equal(typeof Temporal.now.dateTimeISO, 'function'));
     it('Temporal.now.date is a function', () => equal(typeof Temporal.now.date, 'function'));
+    it('Temporal.now.dateISO is a function', () => equal(typeof Temporal.now.dateISO, 'function'));
     it('Temporal.now.timeISO is a function', () => equal(typeof Temporal.now.timeISO, 'function'));
     it('Temporal.now.timeZone is a function', () => equal(typeof Temporal.now.timeZone, 'function'));
   });
   describe('Temporal.now.absolute()', () => {
     it('Temporal.now.instant() returns an Instant', () => assert(Temporal.now.instant() instanceof Temporal.Instant));
+  });
+  describe('Temporal.now.dateTimeISO()', () => {
+    it('returns a DateTime in the ISO calendar', () => {
+      const dt = Temporal.now.dateTimeISO();
+      assert(dt instanceof Temporal.DateTime);
+      equal(dt.calendar.id, 'iso8601');
+    });
   });
   describe('Temporal.now.dateTime()', () => {
     it('returns a DateTime in the correct calendar', () => {
@@ -37,6 +46,12 @@ describe('Temporal.now', () => {
     });
     it('requires a calendar', () => throws(() => Temporal.now.dateTime(), RangeError));
   });
+  describe('Temporal.now.dateISO()', () => {
+    it('returns a Date in the ISO calendar', () => {
+      const d = Temporal.now.dateISO();
+      assert(d instanceof Temporal.Date);
+      equal(d.calendar.id, 'iso8601');
+    });
   });
   describe('Temporal.now.date()', () => {
     it('returns a Date in the correct calendar', () => {
