134b2e2e966f6914340e5cfaf04b2f9a5de166b5
Ujjwal Sharma <ryzokuken@disroot.org>
2021-02-08 14:25:45 +0530

    polyfill: fix Instant precision bug
    Fix a bug where Instant.prototype.toString produced the incorrect result
    where the fractional part included leading zeroes and precision was not
    auto or 0. Includes test262 test for the same.
    
    Fixes: https://github.com/tc39/proposal-temporal/issues/1336

--- polyfill/lib/ecmascript.mjs
+++ polyfill/lib/ecmascript.mjs
@@ -1676,7 +1676,7 @@ export const ES = ObjectAssign({}, ES2020, {
       while (fraction[fraction.length - 1] === '0') fraction = fraction.slice(0, -1);
     } else {
       if (precision === 0) return secs;
-      fraction = `${fraction}`.slice(0, precision).padStart(precision, '0');
+      fraction = `${fraction}`.padStart(9, '0').slice(0, precision);
     }
     return `${secs}.${fraction}`;
   },
