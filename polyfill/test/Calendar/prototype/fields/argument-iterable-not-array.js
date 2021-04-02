79fee277153a3698c63e7d1f9b99b0cf68458750
Philip Chimento <pchimento@igalia.com>
2021-04-02 12:44:36 -0700

    Allow calendar.fields() to accept an iterable
    Temporal will only ever call the fields() method with an array, but other
    callers may want to supply a different iterable (such as a Set).
    
    See: #1427

---
+++ polyfill/test/Calendar/prototype/fields/argument-iterable-not-array.js
@@ -0,0 +1,25 @@
+// Copyright (C) 2021 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.calendar.prototype.fields
+info: |
+    sec-temporal.calendar.prototype.fields step 4:
+      4. Let _fieldNames_ be ? IterableToList(_fields_).
+includes: [compareArray.js]
+---*/
+
+const fieldNames = ["day", "month", "monthCode", "year"];
+const iterable = {
+  iteratorExhausted: false,
+  *[Symbol.iterator]() {
+    yield* fieldNames;
+    this.iteratorExhausted = true;
+  },
+};
+
+const calendar = new Temporal.Calendar("iso8601");
+const result = calendar.fields(iterable);
+
+assert.compareArray(result, fieldNames);
+assert(iterable.iteratorExhausted);
---
+++ polyfill/test/Calendar/prototype/fields/non-string-element-throws.js
@@ -0,0 +1,15 @@
+// Copyright (C) 2021 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.calendar.prototype.fields
+info: |
+    sec-temporal.calendar.prototype.fields step 5:
+      5. For each element _fieldName_ of _fieldNames_, do
+        a. If Type(_fieldName_) is not String, throw a *TypeError* exception.
+---*/
+
+const calendar = new Temporal.Calendar("iso8601");
+[true, 3, 3n, {}, () => {}, Symbol(), undefined, null].forEach((element) => {
+  assert.throws(TypeError, () => calendar.fields([element]), "bad input to calendar.fields()");
+});
