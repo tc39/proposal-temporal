1f92033d1eb8d6b8f82781304eb6aa1ae89d3323
Philip Chimento <pchimento@igalia.com>
2021-04-28 17:48:46 -0700

    Editorial: Consolidate GetOption usage
    Under the assumption that it's not a priority to move GetNumberOption
    directly from ECMA-402 into ECMA-262, reorganize the GetOption operations:
    
    - Change the _type_ string argument to GetOption, into a List of valid
      types, the last one of which will be coerced to if the value is none of
      them (and also not undefined).
    
    - In the Number case, call ToNumber on the value and throw if it is NaN.
    
    - Inline GetNumberOption as it's only called once.
    
    - Call GetOption in GetStringOrNumberOption.
    
    - Don't delete GetOption, GetNumberOption, and DefaultNumberOption from
      402, as they won't be replaced by anything in 262 at present.
    
    Includes a fix to make the polyfill spec-compliant, and test262 tests
    covering usages of GetOption (there are quite a lot of them.)
    
    Closes: #1411

---
+++ polyfill/test/ZonedDateTime/prototype/toString/smallestunit-wrong-type.js
@@ -0,0 +1,14 @@
+// Copyright (C) 2021 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.zoneddatetime.prototype.tostring
+description: Type conversions for smallestUnit option
+includes: [compareArray.js, temporalHelpers.js]
+---*/
+
+const datetime = new Temporal.ZonedDateTime(1_000_000_000_123_987_500n, "UTC");
+TemporalHelpers.checkStringOptionWrongType("smallestUnit", "microsecond",
+  (smallestUnit) => datetime.toString({ smallestUnit }),
+  (result, descr) => assert.sameValue(result, "2001-09-09T01:46:40.123987+00:00[UTC]", descr),
+);
