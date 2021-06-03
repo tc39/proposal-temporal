2c8aa80362c270b90ccb16caae3807fded3101f2
Ms2ger <Ms2ger@gmail.com>
2021-06-03 17:31:26 +0200

    Polyfill: Add missing brand checks in TimeZone.

---
+++ polyfill/test/TimeZone/prototype/getOffsetStringFor/branding.js
@@ -0,0 +1,21 @@
+// Copyright (C) 2021 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.timezone.prototype.getoffsetstringfor
+features: [Symbol]
+---*/
+
+const getOffsetStringFor = Temporal.TimeZone.prototype.getOffsetStringFor;
+
+assert.sameValue(typeof getOffsetStringFor, "function");
+
+assert.throws(TypeError, () => getOffsetStringFor.call(undefined), "undefined");
+assert.throws(TypeError, () => getOffsetStringFor.call(null), "null");
+assert.throws(TypeError, () => getOffsetStringFor.call(true), "true");
+assert.throws(TypeError, () => getOffsetStringFor.call(""), "empty string");
+assert.throws(TypeError, () => getOffsetStringFor.call(Symbol()), "symbol");
+assert.throws(TypeError, () => getOffsetStringFor.call(1), "1");
+assert.throws(TypeError, () => getOffsetStringFor.call({}), "plain object");
+assert.throws(TypeError, () => getOffsetStringFor.call(Temporal.TimeZone), "Temporal.TimeZone");
+assert.throws(TypeError, () => getOffsetStringFor.call(Temporal.TimeZone.prototype), "Temporal.TimeZone.prototype");
