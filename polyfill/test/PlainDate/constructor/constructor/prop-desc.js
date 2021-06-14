279c01a292869fc91e20c0a5687c72718dd769ac
Ms2ger <Ms2ger@gmail.com>
2021-06-14 16:29:28 +0200

    Add tests for the shape of constructors.

---
+++ polyfill/test/PlainDate/constructor/constructor/prop-desc.js
@@ -0,0 +1,20 @@
+// Copyright (C) 2021 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.plaindate
+includes: [propertyHelper.js]
+features: [Temporal]
+---*/
+
+assert.sameValue(
+  typeof Temporal.PlainDate,
+  "function",
+  "`typeof PlainDate` is `function`"
+);
+
+verifyProperty(Temporal, "PlainDate", {
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
