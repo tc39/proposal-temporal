1f87a4610aa5a7d7d7e28be03f2e644a83615df4
Ms2ger <Ms2ger@gmail.com>
2021-06-15 11:03:55 +0200

    Test that constructors need to be called with 'new'.

---
+++ polyfill/test/Duration/constructor/constructor/constructor.js
@@ -0,0 +1,11 @@
+// Copyright (C) 2021 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.duration
+info: |
+    1. If NewTarget is undefined, throw a TypeError exception.
+features: [Temporal]
+---*/
+
+assert.throws(TypeError, () => Temporal.Duration());
