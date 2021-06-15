460104421b99f34ef45e76b1653aa9f5819b5f8c
Ms2ger <Ms2ger@gmail.com>
2021-06-15 16:21:50 +0200

    Improve time-undefined.js.

--- polyfill/test/PlainDate/prototype/toPlainDateTime/time-undefined.js
+++ polyfill/test/PlainDate/prototype/toPlainDateTime/time-undefined.js
@@ -3,23 +3,14 @@
 
 /*---
 esid: sec-temporal.plaindate.prototype.toplaindatetime
+includes: [temporalHelpers.js]
 features: [Temporal]
 ---*/
 
 const date = new Temporal.PlainDate(2000, 5, 2);
 
 const explicit = date.toPlainDateTime(undefined);
-assert.sameValue(explicit.hour, 0, "default time is midnight");
-assert.sameValue(explicit.minute, 0, "default time is midnight");
-assert.sameValue(explicit.second, 0, "default time is midnight");
-assert.sameValue(explicit.millisecond, 0, "default time is midnight");
-assert.sameValue(explicit.microsecond, 0, "default time is midnight");
-assert.sameValue(explicit.nanosecond, 0, "default time is midnight");
+TemporalHelpers.assertPlainDateTime(explicit, 2000, 5, "M05", 2, 0, 0, 0, 0, 0, 0, "default time is midnight - explicit");
 
 const implicit = date.toPlainDateTime();
-assert.sameValue(implicit.hour, 0, "default time is midnight");
-assert.sameValue(implicit.minute, 0, "default time is midnight");
-assert.sameValue(implicit.second, 0, "default time is midnight");
-assert.sameValue(implicit.millisecond, 0, "default time is midnight");
-assert.sameValue(implicit.microsecond, 0, "default time is midnight");
-assert.sameValue(implicit.nanosecond, 0, "default time is midnight");
+TemporalHelpers.assertPlainDateTime(implicit, 2000, 5, "M05", 2, 0, 0, 0, 0, 0, 0, "default time is midnight - implicit");
