801b23041c8e6d9ed00d792ac7ae64f4de54b576
ptomato <philip.chimento@gmail.com>
2021-06-15 17:01:02 +0000

    Deploying to gh-pages from  @ fc3d98af6e2f82a734ac3eaad505eed55bc81fb2 🚀

--- docs/playground.js
+++ docs/playground.js
@@ -152,7 +152,7 @@
   }
 
   function _iterableToArrayLimit(arr, i) {
-    var _i = arr && (typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]);
+    var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"];
 
     if (_i == null) return;
     var _arr = [];
