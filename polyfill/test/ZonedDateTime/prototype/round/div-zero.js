ba3c249b11804819c98f4e07bbac7e40e8354f16
Ms2ger <Ms2ger@gmail.com>
2021-06-28 14:48:50 +0200

    Editorial: Avoid dividing by zero in ZonedDateTime#round.
    Ref. #1502.

---
+++ polyfill/test/ZonedDateTime/prototype/round/div-zero.js
@@ -0,0 +1,23 @@
+// Copyright (C) 2021 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.zoneddatetime.prototype.round
+features: [Temporal]
+---*/
+
+class Calendar extends Temporal.Calendar {
+  constructor() {
+    super("iso8601");
+  }
+  dateAdd(d) {
+    return d;
+  }
+}
+
+const zdt = new Temporal.ZonedDateTime(0n, "UTC", new Calendar());
+
+const units = ["day", "hour", "minute", "second", "millisecond", "microsecond", "nanosecond"];
+for (const smallestUnit of units) {
+  assert.throws(RangeError, () => zdt.round({ smallestUnit }));
+}
