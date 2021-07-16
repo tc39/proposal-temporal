4b4dbd427d4b0468a3b064ca7082f25b209923bc
Justin Grant <justingrant@users.noreply.github.com>
2021-07-16 02:40:06 -0700

    Fix division issues caught by tsc in RoundDuration
    Port of https://github.com/js-temporal/temporal-polyfill/pull/14

--- polyfill/lib/ecmascript.mjs
+++ polyfill/lib/ecmascript.mjs
@@ -3900,8 +3900,8 @@ export const ES = ObjectAssign({}, ES2020, {
         oneYearDays = MathAbs(oneYearDays);
         const divisor = bigInt(oneYearDays).multiply(dayLengthNs);
         nanoseconds = divisor.multiply(years).plus(bigInt(days).multiply(dayLengthNs)).plus(nanoseconds);
-        const rounded = ES.RoundNumberToIncrement(nanoseconds, divisor * increment, roundingMode);
-        total = nanoseconds.toJSNumber() / divisor;
+        const rounded = ES.RoundNumberToIncrement(nanoseconds, divisor.multiply(increment).toJSNumber(), roundingMode);
+        total = nanoseconds.toJSNumber() / divisor.toJSNumber();
         years = rounded.divide(divisor).toJSNumber();
         nanoseconds = months = weeks = days = 0;
         break;
@@ -3942,8 +3942,8 @@ export const ES = ObjectAssign({}, ES2020, {
         oneMonthDays = MathAbs(oneMonthDays);
         const divisor = bigInt(oneMonthDays).multiply(dayLengthNs);
         nanoseconds = divisor.multiply(months).plus(bigInt(days).multiply(dayLengthNs)).plus(nanoseconds);
-        const rounded = ES.RoundNumberToIncrement(nanoseconds, divisor * increment, roundingMode);
-        total = nanoseconds.toJSNumber() / divisor;
+        const rounded = ES.RoundNumberToIncrement(nanoseconds, divisor.multiply(increment).toJSNumber(), roundingMode);
+        total = nanoseconds.toJSNumber() / divisor.toJSNumber();
         months = rounded.divide(divisor).toJSNumber();
         nanoseconds = weeks = days = 0;
         break;
@@ -3964,8 +3964,8 @@ export const ES = ObjectAssign({}, ES2020, {
         oneWeekDays = MathAbs(oneWeekDays);
         const divisor = bigInt(oneWeekDays).multiply(dayLengthNs);
         nanoseconds = divisor.multiply(weeks).plus(bigInt(days).multiply(dayLengthNs)).plus(nanoseconds);
-        const rounded = ES.RoundNumberToIncrement(nanoseconds, divisor * increment, roundingMode);
-        total = nanoseconds.toJSNumber() / divisor;
+        const rounded = ES.RoundNumberToIncrement(nanoseconds, divisor.multiply(increment).toJSNumber(), roundingMode);
+        total = nanoseconds.toJSNumber() / divisor.toJSNumber();
         weeks = rounded.divide(divisor).toJSNumber();
         nanoseconds = days = 0;
         break;
@@ -3973,8 +3973,8 @@ export const ES = ObjectAssign({}, ES2020, {
       case 'day': {
         const divisor = bigInt(dayLengthNs);
         nanoseconds = divisor.multiply(days).plus(nanoseconds);
-        const rounded = ES.RoundNumberToIncrement(nanoseconds, divisor * increment, roundingMode);
-        total = nanoseconds.toJSNumber() / divisor;
+        const rounded = ES.RoundNumberToIncrement(nanoseconds, divisor.multiply(increment).toJSNumber(), roundingMode);
+        total = nanoseconds.toJSNumber() / divisor.toJSNumber();
         days = rounded.divide(divisor).toJSNumber();
         nanoseconds = 0;
         break;
