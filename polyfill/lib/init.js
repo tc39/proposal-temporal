b8454bd55e6ab2e2818006a8f98a2fcef29c7acd
Philip Chimento <pchimento@igalia.com>
2021-08-03 11:21:10 -0700

    Fix 'npm run playground'
    This is still useful when debugging the tests, so let's keep it working.

--- polyfill/lib/init.js
+++ polyfill/lib/init.js
@@ -3,18 +3,7 @@ if (module.parent && module.parent.id === 'internal/preload') {
   globalThis.__debug__ = true;
 }
 
-import('./index.mjs')
-  .then(({ Temporal, Intl, toTemporalInstant }) => {
-    globalThis.Temporal = { ...Temporal };
-    Object.assign(globalThis.Intl, Intl);
-    Object.defineProperty(Date.prototype, 'toTemporalInstant', {
-      value: toTemporalInstant,
-      writable: true,
-      enumerable: false,
-      configurable: true
-    });
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+import('./shim.mjs').catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
