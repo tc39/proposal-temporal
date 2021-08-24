f03ffd7eae986d1b86cf03e17dc71891d8919d00
Philip Chimento <pchimento@igalia.com>
2021-08-24 12:23:55 -0700

    (BROKEN) Extend datemath tests to 'gregory' calendar

--- polyfill/test/datemath.mjs
+++ polyfill/test/datemath.mjs
@@ -16,49 +16,54 @@ const { equal } = assert;
 
 import * as Temporal from 'proposal-temporal';
 
-describe('Date.since(simple, simple)', () => {
-  build('Before Leap Day', '2020-01-03', '2020-02-15');
-  build('Before Leap Day', '2020-01-28', '2020-02-15');
-  build('Before Leap Day', '2020-01-31', '2020-02-15');
-  build('Cross Leap Day', '2020-01-31', '2020-06-30');
-  build('After Leap Day', '2020-03-31', '2020-06-30');
-  build('After Leap Day', '2020-03-25', '2020-07-31');
-});
-describe('Date.since(normal, normal)', () => {
-  build('Month<2 & Month<2', '2018-01-20', '2019-01-05');
-  build('Month>2 & Month>2', '2018-03-20', '2019-03-05');
-  build('Month>2 & Month>2', '2018-04-20', '2019-04-05');
-  build('Month<2 & Month>2', '2018-01-20', '2019-04-05');
-  build('Month>2 & Month<2', '2018-03-20', '2019-01-05');
-  build('Month>2 & Month<2', '2018-04-20', '2019-01-05');
-});
-describe('Date.since(leap, leap)', () => {
-  build('Month<2 & Month<2', '2016-01-20', '2020-01-05');
-  build('Month>2 & Month>2', '2016-03-20', '2020-04-05');
-  build('Month>2 & Month>2', '2016-03-20', '2020-03-05');
-  build('Month<2 & Month>2', '2016-01-20', '2020-02-05');
-  build('Month>2 & Month<2', '2016-03-20', '2020-01-05');
-  build('Month>2 & Month<2', '2016-04-20', '2020-01-05');
-});
-describe('Date.since(leap, normal)', () => {
-  build('Month<2 & Month<2', '2016-01-20', '2017-01-05');
-  build('Month>2 & Month>2', '2016-03-20', '2017-04-05');
-  build('Month>2 & Month>2', '2016-04-20', '2017-03-05');
-  build('Month<2 & Month>2', '2016-01-20', '2017-04-05');
-  build('Month>2 & Month<2', '2016-03-20', '2017-01-05');
-  build('Month>2 & Month<2', '2016-04-20', '2017-01-05');
-});
-describe('Date.since(normal, leap)', () => {
-  build('Month<2 & Month<2', '2019-01-20', '2020-01-05');
-  build('Month>2 & Month>2', '2019-03-20', '2020-04-05');
-  build('Month>2 & Month>2', '2019-04-20', '2020-03-05');
-  build('Month<2 & Month>2', '2019-01-20', '2020-04-05');
-  build('Month>2 & Month<2', '2019-03-20', '2020-01-05');
-  build('Month>2 & Month<2', '2019-04-20', '2020-01-05');
+['iso8601', 'gregory'].forEach((cal) => {
+  describe(`${cal} Date.since(simple, simple)`, () => {
+    build('Before Leap Day', '2020-01-03', '2020-02-15', cal);
+    build('Before Leap Day', '2020-01-28', '2020-02-15', cal);
+    build('Before Leap Day', '2020-01-31', '2020-02-15', cal);
+    build('Cross Leap Day', '2020-01-31', '2020-06-30', cal);
+    build('After Leap Day', '2020-03-31', '2020-06-30', cal);
+    build('After Leap Day', '2020-03-25', '2020-07-31', cal);
+  });
+  describe(`${cal} Date.since(normal, normal)`, () => {
+    build('Month<2 & Month<2', '2018-01-20', '2019-01-05', cal);
+    build('Month>2 & Month>2', '2018-03-20', '2019-03-05', cal);
+    build('Month>2 & Month>2', '2018-04-20', '2019-04-05', cal);
+    build('Month<2 & Month>2', '2018-01-20', '2019-04-05', cal);
+    build('Month>2 & Month<2', '2018-03-20', '2019-01-05', cal);
+    build('Month>2 & Month<2', '2018-04-20', '2019-01-05', cal);
+  });
+  describe(`${cal} Date.since(leap, leap)`, () => {
+    build('Month<2 & Month<2', '2016-01-20', '2020-01-05', cal);
+    build('Month>2 & Month>2', '2016-03-20', '2020-04-05', cal);
+    build('Month>2 & Month>2', '2016-03-20', '2020-03-05', cal);
+    build('Month<2 & Month>2', '2016-01-20', '2020-02-05', cal);
+    build('Month>2 & Month<2', '2016-03-20', '2020-01-05', cal);
+    build('Month>2 & Month<2', '2016-04-20', '2020-01-05', cal);
+  });
+  describe(`${cal} Date.since(leap, normal)`, () => {
+    build('Month<2 & Month<2', '2016-01-20', '2017-01-05', cal);
+    build('Month>2 & Month>2', '2016-03-20', '2017-04-05', cal);
+    build('Month>2 & Month>2', '2016-04-20', '2017-03-05', cal);
+    build('Month<2 & Month>2', '2016-01-20', '2017-04-05', cal);
+    build('Month>2 & Month<2', '2016-03-20', '2017-01-05', cal);
+    build('Month>2 & Month<2', '2016-04-20', '2017-01-05', cal);
+  });
+  describe(`${cal} Date.since(normal, leap)`, () => {
+    build('Month<2 & Month<2', '2019-01-20', '2020-01-05', cal);
+    build('Month>2 & Month>2', '2019-03-20', '2020-04-05', cal);
+    build('Month>2 & Month>2', '2019-04-20', '2020-03-05', cal);
+    build('Month<2 & Month>2', '2019-01-20', '2020-04-05', cal);
+    build('Month>2 & Month<2', '2019-03-20', '2020-01-05', cal);
+    build('Month>2 & Month<2', '2019-04-20', '2020-01-05', cal);
+  });
 });
 
-function build(name, sone, stwo) {
-  const [one, two] = [Temporal.PlainDate.from(sone), Temporal.PlainDate.from(stwo)].sort(Temporal.PlainDate.compare);
+function build(name, sone, stwo, cal) {
+  const [one, two] = [
+    Temporal.PlainDate.from(sone).withCalendar(cal),
+    Temporal.PlainDate.from(stwo).withCalendar(cal)
+  ].sort(Temporal.PlainDate.compare);
   describe(name, () => {
     const largestUnits = ['years', 'months', 'weeks', 'days'];
     buildSub(one, two, largestUnits);
