eacdfd270386c0ae19da958ca99af0eae986620c
Ms2ger <Ms2ger@gmail.com>
2021-09-22 15:29:44 +0000

    Deploying to gh-pages from  @ bece5e8845cb7347214c70257fa6feb0cf323364 🚀

--- docs/playground.js
+++ docs/playground.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.temporal = {}));
-}(this, (function (exports) { 'use strict';
+})(this, (function (exports) { 'use strict';
 
   function ownKeys(object, enumerableOnly) {
     var keys = Object.keys(object);
@@ -15394,5 +15394,5 @@
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));
 //# sourceMappingURL=playground.js.map
