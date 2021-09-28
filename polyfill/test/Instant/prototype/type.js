8cac78924d74f03056d5a98d5ecbf34bdd102220
Philip Chimento <pchimento@igalia.com>
2021-09-28 07:55:47 -0700

    Remove duplicated tests
    These tests are already covered by other test files in the same directory.

--- polyfill/test/Instant/prototype/type.js
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2020 Igalia, S.L. All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-
-const { Instant } = Temporal;
-
-assert.sameValue(typeof Instant.prototype, "object");
-assert.notSameValue(Instant.prototype, null);
