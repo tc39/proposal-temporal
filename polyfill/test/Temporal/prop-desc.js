4cf62fa18d75e60459392c158f34c57730cae2dd
Philip Chimento <pchimento@igalia.com>
2021-09-28 07:52:12 -0700

    Add esid to all test262 tests
    There were a few tests that were still missing it.

--- polyfill/test/Temporal/prop-desc.js
+++ polyfill/test/Temporal/prop-desc.js
@@ -2,6 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
+esid: sec-temporal-objects
 includes: [propertyHelper.js]
 description: The "Temporal" property of the global object
 features: [Temporal]
