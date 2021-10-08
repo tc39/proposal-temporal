65497078007ec2431c12389a2c6ac3cecb659ae4
Philip Chimento <pchimento@igalia.com>
2021-10-08 10:58:39 -0700

    Normative: Throw earlier in ZonedDateTime.with if offset field is missing
    As discussed in https://github.com/tc39/proposal-temporal/pull/1762#discussion_r695948180
    
    This changes ZonedDateTime.prototype.with() to throw slightly earlier
    (before calling calendar methods in user code) in the unlikely case that
    the offset field is messed up or missing.

--- polyfill/lib/zoneddatetime.mjs
+++ polyfill/lib/zoneddatetime.mjs
@@ -206,9 +206,29 @@ export class ZonedDateTime {
     if (!props) {
       throw new TypeError('invalid zoned-date-time-like');
     }
-    let fields = ES.ToTemporalZonedDateTimeFields(this, fieldNames);
+    const entries = [
+      ['day', undefined],
+      ['hour', 0],
+      ['microsecond', 0],
+      ['millisecond', 0],
+      ['minute', 0],
+      ['month', undefined],
+      ['monthCode', undefined],
+      ['nanosecond', 0],
+      ['second', 0],
+      ['year', undefined],
+      ['offset'],
+      ['timeZone']
+    ];
+    // Add extra fields from the calendar at the end
+    fieldNames.forEach((fieldName) => {
+      if (!entries.some(([name]) => name === fieldName)) {
+        entries.push([fieldName, undefined]);
+      }
+    });
+    let fields = ES.PrepareTemporalFields(this, entries);
     fields = ES.CalendarMergeFields(calendar, fields, props);
-    fields = ES.ToTemporalZonedDateTimeFields(fields, fieldNames);
+    fields = ES.PrepareTemporalFields(fields, entries);
     let { year, month, day, hour, minute, second, millisecond, microsecond, nanosecond } =
       ES.InterpretTemporalDateTimeFields(calendar, fields, options);
     const offsetNs = ES.ParseOffsetString(fields.offset);
