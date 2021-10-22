669e13613393ea2c83d1141f7dd4a71a2fdb663f
Philip Chimento <pchimento@igalia.com>
2021-10-22 14:50:35 -0700

    Fix cookbook tests with new module loader API
    Node changed its module loader API (which is unstable, so, fair enough)
    and we need to adapt our cookbook tests, which used the transformSource
    hook. Instead of adapting directly to the new API, I found there was an
    easier way to achieve the same thing, using the globalPreload hook (called
    getGlobalPreloadCode in earlier versions.)
    
    Closes: #1882

--- polyfill/test/resolve.cookbook.mjs
+++ polyfill/test/resolve.cookbook.mjs
@@ -3,22 +3,16 @@
  ** This code is governed by the license found in the LICENSE file.
  */
 
-import fs from 'fs';
-const PKG = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
-export function resolve(specifier, parent, defaultResolve) {
-  if (specifier === PKG.name) {
-    specifier = new URL('../lib/shim.mjs', import.meta.url).toString();
-  }
-  return defaultResolve(specifier, parent);
+// Current Node.js
+export function globalPreload() {
+  return `\
+const { createRequire } = getBuiltin('module');
+const { cwd } = getBuiltin('process');
+const require = createRequire(cwd() + '/<preload>');
+require('./script.js');
+globalThis.assert = getBuiltin('assert').strict;
+`;
 }
 
-export async function transformSource(source, { url, format }, defaultTransformSource) {
-  if (typeof source !== 'string') source = source.toString();
-  if (url !== 'all.mjs' && !url.endsWith('polyfill/lib/index.mjs')) {
-    return {
-      source: `import '${PKG.name}';import assert from 'assert';` + source
-    };
-  } else {
-    return defaultTransformSource(source, { url, format }, defaultTransformSource);
-  }
-}
+// LTS Node.js
+export const getGlobalPreloadCode = globalPreload;
