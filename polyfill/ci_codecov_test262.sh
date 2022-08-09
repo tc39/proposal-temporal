c945cb24b6b6b6df6a29b12c184741e6118a489b
Philip Chimento <pchimento@igalia.com>
2022-08-09 11:34:36 -0700

    Add faster test262 runner
    See comments in polyfill/test262-harness.mjs. This speeds up a test262 run
    from several minutes down to 45 seconds on my computer, because we can
    reuse the polyfill's compiled bytecode for each test; that's not supported
    by the test262-harness module.
    
    Gathering code coverage is also faster because we can use Node's native
    code coverage instead of instrumenting the polyfill with nyc. However,
    there is a memory leak in my version of Node when using vm.Script with
    code coverage, so we have to break the test suite up in
    ci_codecov_test262.sh when collecting coverage or else we run out of
    memory.

--- polyfill/ci_codecov_test262.sh
+++ polyfill/ci_codecov_test262.sh
@@ -7,6 +7,33 @@ if [ -n "$HEAD_SHA" ]; then
   SHA_ARG="-c $HEAD_SHA"
 fi
 
-export COVERAGE=yes
-npm run test262
+npm run build262
+
+export NODE_V8_COVERAGE=coverage/tmp/
+rm -rf $NODE_V8_COVERAGE
+
+# Run the tests in chunks, by subdirectory
+# Works around memory leak with vm.Script and NODE_V8_COVERAGE
+subdirs=$(find test262/test/*/Temporal/* -maxdepth 0 -type d -exec basename "{}" ";" | sort -u)
+
+failed=0
+node runtest262.mjs "*.js" || failed=1
+# does not set failed=1 because that directory currently doesn't exist:
+node runtest262.mjs "test262/test/built-ins/Date/*/toTemporalInstant/*.js" || true
+for subdir in $subdirs; do
+  node runtest262.mjs "$subdir/**" || failed=1
+done
+node runtest262.mjs "test262/test/staging/Intl402/Temporal/**/*.js" || failed=1
+
+c8 report --reporter=text-lcov --temp-directory=$NODE_V8_COVERAGE \
+  --exclude=polyfill/runtest262.mjs \
+  --exclude-after-remap > coverage/test262.lcov
+
 codecov $SHA_ARG -f coverage/test262.lcov
+
+rm -rf $NODE_V8_COVERAGE
+
+if [ $failed -ne 0 ]; then
+  echo Some tests failed.
+  exit 1
+fi
