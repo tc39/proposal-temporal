714c72ec62ab88a232d0a9afdcd76d4e6cff4aad
Philip Chimento <pchimento@igalia.com>
2022-08-09 11:34:36 -0700

    Add faster test262 runner
    See comments in polyfill/test262-harness.mjs. This speeds up a test262 run
    from several minutes down to 45 seconds on my computer, because we can
    reuse the polyfill's compiled bytecode for each test; that's not supported
    by the test262-harness module.
    
    Gathering code coverage is also faster because we can use Node's native
    code coverage instead of instrumenting the polyfill with nyc. However,
    there is a memory leak in my version of Node when using vm.Script with
    code coverage, so we have to break the test suite up in
    ci_codecov_test262.sh when collecting coverage or else we run out of
    memory.

--- polyfill/test/transform.test262.js
+++
@@ -1,9 +0,0 @@
-module.exports = function addCoverageOutput(code) {
-  return `${code}
-const fs = require('fs');
-const {v4: uuid} = require('uuid');
-const filename = '../../coverage/tmp/transformer/' + uuid() + '.json';
-fs.mkdirSync('../../coverage/tmp/transformer/', { recursive: true });
-fs.writeFileSync(filename, JSON.stringify(globalThis.__coverage__ || {}), 'utf-8');
-`;
-};
