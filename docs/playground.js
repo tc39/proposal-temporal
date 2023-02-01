ee64089f30cfc940f55eea792684c62cd0a88e5a
ptomato <philip.chimento@gmail.com>
2023-02-01 19:58:02 +0000

    Deploying to gh-pages from  @ 0bb12c2ae14df4e191fc09c80df02b3476bbe375 🚀

--- docs/playground.js
+++ docs/playground.js
@@ -8258,10 +8258,14 @@
       var increment = options.roundingIncrement;
       if (increment === undefined) return 1;
       increment = ES.ToNumber(increment);
-      if (!NumberIsFinite(increment) || increment < 1) {
-        throw new RangeError("roundingIncrement must be at least 1 and finite, not ".concat(increment));
+      if (!NumberIsFinite(increment)) {
+        throw new RangeError('roundingIncrement must be finite');
       }
-      return MathTrunc(increment);
+      var integerIncrement = MathTrunc(increment);
+      if (integerIncrement < 1 || integerIncrement > 1e9) {
+        throw new RangeError("roundingIncrement must be at least 1 and at most 1e9, not ".concat(increment));
+      }
+      return integerIncrement;
     },
     ValidateTemporalRoundingIncrement: function ValidateTemporalRoundingIncrement(increment, dividend, inclusive) {
       var maximum = inclusive ? dividend : dividend - 1;
