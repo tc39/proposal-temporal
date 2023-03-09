411418150dd2c1820d5660cba1ff8fca7eafe96a
Philip Chimento <pchimento@igalia.com>
2023-03-08 16:11:18 -0800

    Normative: Avoid calendar operations when adding days-only duration to ZDT
    We call AddZonedDateTime in several places with all of the duration
    components being zero except for days. In this case, we can skip the
    calendar operations altogether, because adding/subtracting days to/from
    the ISO fields is not calendar-dependent.

--- polyfill/lib/zoneddatetime.mjs
+++ polyfill/lib/zoneddatetime.mjs
@@ -389,10 +389,10 @@ export class ZonedDateTime {
     const calendar = GetSlot(this, CALENDAR);
     const dtStart = ES.CreateTemporalDateTime(year, month, day, 0, 0, 0, 0, 0, 0, 'iso8601');
     const instantStart = ES.GetInstantFor(timeZone, dtStart, 'compatible');
-    const endNs = ES.AddZonedDateTime(instantStart, timeZone, calendar, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, dtStart);
+    const endNs = ES.AddDaysToZonedDateTime(instantStart, dtStart, timeZone, calendar, 1).epochNs;
     const dayLengthNs = endNs.subtract(GetSlot(instantStart, EPOCHNANOSECONDS));
     if (dayLengthNs.leq(0)) {
-      throw new RangeError('cannot round a ZonedDateTime in a calendar with zero- or negative-length days');
+      throw new RangeError('cannot round a ZonedDateTime in a time zone with zero- or negative-length days');
     }
     ({ year, month, day, hour, minute, second, millisecond, microsecond, nanosecond } = ES.RoundISODateTime(
       year,
