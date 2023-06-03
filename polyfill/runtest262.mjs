88b406fbad0f6b7b24643651f5bccb39d2910727
Justin Grant <justingrant@users.noreply.github.com>
2023-06-02 17:54:51 -0700

    Fix Test262 timeout for debugging
    A previous commit extended the timeout for running Test262 tests because
    a few of our tests now take as long as 10-20 seconds to run. However,
    the right place to extend the timeout is in `runTest262.mjs`, so that
    developers can add a timeout on the command line, for example:
    
    ```sh
    TIMEOUT=3600000 npm run test262
    ```
    
    This is important when running Test262 under a debugger where
    timeouts need to be much longer than 30 seconds.

--- polyfill/runtest262.mjs
+++ polyfill/runtest262.mjs
@@ -4,7 +4,7 @@ const result = runTest262({
   test262Dir: 'test262',
   polyfillCodeFile: 'script.js',
   expectedFailureFiles: ['test/expected-failures.txt'],
-  timeoutMsecs: process.env.TIMEOUT,
+  timeoutMsecs: process.env.TIMEOUT || 30000,
   testGlobs: process.argv.slice(2)
 });
 
