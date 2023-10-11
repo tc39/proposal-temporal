22ca50352d993500647d2e026d38331ff58d5607
Philip Chimento <pchimento@igalia.com>
2023-10-11 15:44:01 -0700

    Polyfill: Implement CompareISODateTime and CompareTemporalTime as in spec
    Change the reference code so that these operations work exactly like in
    the spec text, instead of the looping implementation that the code
    previously had.

--- polyfill/lib/plaindatetime.mjs
+++ polyfill/lib/plaindatetime.mjs
@@ -355,15 +355,30 @@ export class PlainDateTime {
   equals(other) {
     if (!ES.IsTemporalDateTime(this)) throw new TypeError('invalid receiver');
     other = ES.ToTemporalDateTime(other);
-    if (GetSlot(this, ISO_YEAR) !== GetSlot(other, ISO_YEAR)) return false;
-    if (GetSlot(this, ISO_MONTH) !== GetSlot(other, ISO_MONTH)) return false;
-    if (GetSlot(this, ISO_DAY) !== GetSlot(other, ISO_DAY)) return false;
-    if (GetSlot(this, ISO_HOUR) !== GetSlot(other, ISO_HOUR)) return false;
-    if (GetSlot(this, ISO_MINUTE) !== GetSlot(other, ISO_MINUTE)) return false;
-    if (GetSlot(this, ISO_SECOND) !== GetSlot(other, ISO_SECOND)) return false;
-    if (GetSlot(this, ISO_MILLISECOND) !== GetSlot(other, ISO_MILLISECOND)) return false;
-    if (GetSlot(this, ISO_MICROSECOND) !== GetSlot(other, ISO_MICROSECOND)) return false;
-    if (GetSlot(this, ISO_NANOSECOND) !== GetSlot(other, ISO_NANOSECOND)) return false;
+    if (
+      ES.CompareISODateTime(
+        GetSlot(this, ISO_YEAR),
+        GetSlot(this, ISO_MONTH),
+        GetSlot(this, ISO_DAY),
+        GetSlot(this, ISO_HOUR),
+        GetSlot(this, ISO_MINUTE),
+        GetSlot(this, ISO_SECOND),
+        GetSlot(this, ISO_MILLISECOND),
+        GetSlot(this, ISO_MICROSECOND),
+        GetSlot(this, ISO_NANOSECOND),
+        GetSlot(other, ISO_YEAR),
+        GetSlot(other, ISO_MONTH),
+        GetSlot(other, ISO_DAY),
+        GetSlot(other, ISO_HOUR),
+        GetSlot(other, ISO_MINUTE),
+        GetSlot(other, ISO_SECOND),
+        GetSlot(other, ISO_MILLISECOND),
+        GetSlot(other, ISO_MICROSECOND),
+        GetSlot(other, ISO_NANOSECOND)
+      ) !== 0
+    ) {
+      return false;
+    }
     return ES.CalendarEquals(GetSlot(this, CALENDAR), GetSlot(other, CALENDAR));
   }
   toString(options = undefined) {
@@ -461,24 +476,26 @@ export class PlainDateTime {
   static compare(one, two) {
     one = ES.ToTemporalDateTime(one);
     two = ES.ToTemporalDateTime(two);
-    const slots = [
-      ISO_YEAR,
-      ISO_MONTH,
-      ISO_DAY,
-      ISO_HOUR,
-      ISO_MINUTE,
-      ISO_SECOND,
-      ISO_MILLISECOND,
-      ISO_MICROSECOND,
-      ISO_NANOSECOND
-    ];
-    for (let index = 0; index < slots.length; index++) {
-      const slot = slots[index];
-      const val1 = GetSlot(one, slot);
-      const val2 = GetSlot(two, slot);
-      if (val1 !== val2) return ES.ComparisonResult(val1 - val2);
-    }
-    return 0;
+    return ES.CompareISODateTime(
+      GetSlot(one, ISO_YEAR),
+      GetSlot(one, ISO_MONTH),
+      GetSlot(one, ISO_DAY),
+      GetSlot(one, ISO_HOUR),
+      GetSlot(one, ISO_MINUTE),
+      GetSlot(one, ISO_SECOND),
+      GetSlot(one, ISO_MILLISECOND),
+      GetSlot(one, ISO_MICROSECOND),
+      GetSlot(one, ISO_NANOSECOND),
+      GetSlot(two, ISO_YEAR),
+      GetSlot(two, ISO_MONTH),
+      GetSlot(two, ISO_DAY),
+      GetSlot(two, ISO_HOUR),
+      GetSlot(two, ISO_MINUTE),
+      GetSlot(two, ISO_SECOND),
+      GetSlot(two, ISO_MILLISECOND),
+      GetSlot(two, ISO_MICROSECOND),
+      GetSlot(two, ISO_NANOSECOND)
+    );
   }
 }
 
