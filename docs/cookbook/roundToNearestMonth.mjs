9d64b8852574b9365623d7776abc4fb99b08c7e0
Andres Kalle <mjomble@gmail.com>
2024-01-15 22:01:12 +0200

    Improve roundToNearestMonth example

--- docs/cookbook/roundToNearestMonth.mjs
+++ docs/cookbook/roundToNearestMonth.mjs
@@ -1,11 +1,12 @@
 const date = Temporal.PlainDate.from('2018-09-16');
 
-let { year, month } = date;
-if ((date.day - 1) / date.daysInMonth >= 0.5) month++;
-if (month > 12) {
-  month %= 12;
-  year++;
-}
-const nearestMonth = Temporal.PlainDate.from({ year, month, day: 1 });
+const firstOfCurrentMonth = date.with({ day: 1 });
+const firstOfNextMonth = firstOfCurrentMonth.add({ months: 1 });
+
+const sinceCurrent = date.since(firstOfCurrentMonth);
+const untilNext = date.until(firstOfNextMonth);
+
+const isCloserToNextMonth = Temporal.Duration.compare(sinceCurrent, untilNext) >= 0;
+const nearestMonth = isCloserToNextMonth ? firstOfNextMonth : firstOfCurrentMonth;
 
 assert.equal(nearestMonth.toString(), '2018-10-01');
