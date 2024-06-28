0576f3f2796be5156c8b366c9c1ab3d93555ff97
Philip Chimento <pchimento@igalia.com>
2024-06-28 23:32:50 +0300

    Polyfill: Improve NudgeToCalendarUnit
    Implements the extra checks and assertions from the previous commit, as
    well as the use of abs() to simplify the didExpandCalendarUnit check and
    to satisfy the requirements of ApplyUnsignedRoundingMode.

--- polyfill/lib/ecmascript.mjs
+++ polyfill/lib/ecmascript.mjs
@@ -3805,6 +3805,10 @@ function NudgeToCalendarUnit(
       throw new Error('assert not reached');
   }
 
+  if ((sign === 1 && (r1 < 0 || r1 >= r2)) || (sign === -1 && (r1 > 0 || r1 <= r2))) {
+    throw new Error('assertion failed: ordering of r1, r2 according to sign');
+  }
+
   // Apply to origin, output PlainDateTimes
   const start = AddDateTime(
     dateTime.year,
@@ -3896,22 +3900,33 @@ function NudgeToCalendarUnit(
   }
 
   // Round the smallestUnit within the epoch-nanosecond span
-  if (endEpochNs.equals(startEpochNs)) {
+  if (
+    (sign === 1 && (startEpochNs.gt(destEpochNs) || destEpochNs.geq(endEpochNs))) ||
+    (sign === -1 && (endEpochNs.geq(destEpochNs) || destEpochNs.gt(startEpochNs)))
+  ) {
     throw new RangeError(`custom calendar reported a ${unit} that is 0 days long`);
+  }
+  if (endEpochNs.equals(startEpochNs)) {
+    throw new Error('assertion failed: startEpochNs ≠ endEpochNs');
   }
   const numerator = TimeDuration.fromEpochNsDiff(destEpochNs, startEpochNs);
   const denominator = TimeDuration.fromEpochNsDiff(endEpochNs, startEpochNs);
   const unsignedRoundingMode = GetUnsignedRoundingMode(roundingMode, sign < 0 ? 'negative' : 'positive');
   const cmp = numerator.add(numerator).abs().subtract(denominator.abs()).sign();
-  const even = (r1 / (increment * sign)) % 2 === 0;
-  const roundedUnit = numerator.isZero() ? r1 : ApplyUnsignedRoundingMode(r1, r2, cmp, even, unsignedRoundingMode);
+  const even = (MathAbs(r1) / increment) % 2 === 0;
+  const roundedUnit = numerator.isZero()
+    ? MathAbs(r1)
+    : ApplyUnsignedRoundingMode(MathAbs(r1), MathAbs(r2), cmp, even, unsignedRoundingMode);
 
   // Trick to minimize rounding error, due to the lack of fma() in JS
   const fakeNumerator = new TimeDuration(denominator.totalNs.times(r1).add(numerator.totalNs.times(increment * sign)));
   const total = fakeNumerator.fdiv(denominator.totalNs);
+  if (MathAbs(total) < MathAbs(r1) || MathAbs(total) >= MathAbs(r2)) {
+    throw new Error('assertion failed: r1 ≤ total < r2');
+  }
 
   // Determine whether expanded or contracted
-  const didExpandCalendarUnit = MathSign(roundedUnit - total) === sign;
+  const didExpandCalendarUnit = roundedUnit === MathAbs(r2);
   duration = didExpandCalendarUnit ? endDuration : startDuration;
 
   return {
