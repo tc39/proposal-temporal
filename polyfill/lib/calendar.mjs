2c1e94e835b44286953d46fd5b3aeb3934234980
Richard Gibson <richard.gibson@gmail.com>
2024-09-30 17:15:47 -0400

    Editorial: Recommend "remapping" for dates exceeding era year bounds
    Fixes #2865

--- polyfill/lib/calendar.mjs
+++ polyfill/lib/calendar.mjs
@@ -1461,9 +1461,6 @@ const makeHelperGregorian = (id, originalEras) => {
           ({ code, names = [] }) => code === era || ES.Call(ArrayPrototypeIncludes, names, [era])
         ]);
         if (!matchingEra) throw new RangeErrorCtor(`Era ${era} (ISO year ${eraYear}) was not matched by any era`);
-        if (eraYear < 1 && matchingEra.reverseOf) {
-          throw new RangeErrorCtor(`Years in ${era} era must be positive, not ${year}`);
-        }
         if (matchingEra.reverseOf) {
           year = matchingEra.anchorEpoch.year - eraYear;
         } else {
