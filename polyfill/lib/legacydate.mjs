c30c8557fcf4aebba74b0445eb3af6d98aed4ec1
Philip Chimento <pchimento@igalia.com>
2024-11-08 11:34:37 -0800

    Polyfill: Make Date.prototype.toTemporalInstant not a constructor
    A test for this was recently added to test262. Per the spec text,
    Date.prototype.toTemporalInstant should not be contstructible. However,
    the previous implementation did allow it to be called as a constructor.

--- polyfill/lib/legacydate.mjs
+++ polyfill/lib/legacydate.mjs
@@ -5,7 +5,18 @@ import { Instant } from './instant.mjs';
 
 import bigInt from 'big-integer';
 
-export function toTemporalInstant() {
-  const epochNanoseconds = bigInt(ES.Call(DatePrototypeValueOf, this, [])).multiply(1e6);
-  return new Instant(ES.BigIntIfAvailable(epochNanoseconds));
+// By default, a plain function can be called as a constructor. A method such as
+// Date.prototype.toTemporalInstant should not be able to. We could check
+// new.target in the body of toTemporalInstant, but that is not sufficient for
+// preventing construction when passing it as the newTarget parameter of
+// Reflect.construct. So we create it as a method of an otherwise unused class,
+// and monkeypatch it onto Date.prototype.
+
+class LegacyDateImpl {
+  toTemporalInstant() {
+    const epochNanoseconds = bigInt(ES.Call(DatePrototypeValueOf, this, [])).multiply(1e6);
+    return new Instant(ES.BigIntIfAvailable(epochNanoseconds));
+  }
 }
+
+export const toTemporalInstant = LegacyDateImpl.prototype.toTemporalInstant;
