4f53c1cfc53d7bcdfa6fa388444da935aa447ae8
Philip Chimento <pchimento@igalia.com>
2025-10-21 16:01:16 -0700

    Thorough ZonedDateTime rounding tests
    Combining the "interesting" ZonedDateTimes from zoneddifference.mjs with
    all possible rounding increments, smallestUnits, and modes.

---
+++ polyfill/test/thorough/zonedrounding.mjs
@@ -0,0 +1,42 @@
+import { getProgressBar, makeZonedCases, roundingModes, time, withSnapshotsFromFile } from './support.mjs';
+
+const roundingGranularities = {
+  day: [1],
+  hour: [1, 2, 3, 4, 6, 8, 12],
+  minute: [1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30],
+  second: [1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30],
+  millisecond: [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500],
+  microsecond: [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500],
+  nanosecond: [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500]
+};
+const totalGranularities = Object.entries(roundingGranularities).reduce(
+  (sum, [, granularities]) => sum + granularities.length,
+  0
+);
+
+const interestingCases = makeZonedCases();
+const total = interestingCases.length * totalGranularities;
+
+await time(async (start) => {
+  const progress = getProgressBar(start, total);
+
+  await withSnapshotsFromFile('./zonedrounding.snapshot.json', (matchSnapshot, matchSnapshotOrOutOfRange) => {
+    for (const [datetime, str] of interestingCases) {
+      for (const [smallestUnit, increments] of Object.entries(roundingGranularities)) {
+        for (const roundingIncrement of increments) {
+          const testName = `${str} ${roundingIncrement} ${smallestUnit}`;
+          progress.tick(1, { test: testName.slice(0, 45) });
+
+          for (const roundingMode of roundingModes) {
+            matchSnapshotOrOutOfRange(
+              () => datetime.round({ smallestUnit, roundingIncrement, roundingMode }),
+              `${testName} ${roundingMode}`
+            );
+          }
+        }
+      }
+    }
+  });
+
+  return total;
+});
