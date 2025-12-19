5d15d2da934af505db333c5b0a311d30b1ffb1f6
Tim Chevalier <tjc@igalia.com>
2025-12-19 12:38:44 -0800

    Polyfill: Change check to assertion in minMaxMonthLength for Hebrew calendar
    This is called on dates that have already been validated, so it should be
    an internal error if there is no length for a valid month code.

--- polyfill/lib/calendar.mjs
+++ polyfill/lib/calendar.mjs
@@ -1274,7 +1274,7 @@ const helperHebrew = makeNonISOHelper([{ code: 'am', isoEpoch: { year: -3760, mo
     const { month, year } = calendarDate;
     const monthCode = calendarDate.monthCode ?? this.getMonthCode(year, month);
     const daysInMonth = this.monthLengths[monthCode];
-    if (daysInMonth === undefined) throw new RangeErrorCtor(`unmatched Hebrew month: ${month}`);
+    assert(daysInMonth, `missing daysInMonth for Hebrew month ${monthCode}`);
     return typeof daysInMonth === 'number' ? daysInMonth : daysInMonth[minOrMax];
   },
   maxLengthOfMonthCodeInAnyYear(monthCode) {
