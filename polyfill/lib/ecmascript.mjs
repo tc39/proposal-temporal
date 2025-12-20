efec08abe891fa747f283f3848b1db9eb3f40278
Tim Chevalier <tjc@igalia.com>
2025-12-19 19:58:39 -0800

    Fix lint error

--- polyfill/lib/ecmascript.mjs
+++ polyfill/lib/ecmascript.mjs
@@ -1267,14 +1267,7 @@ export function ToTemporalInstant(item) {
   }
   const { year, month, day, time, offset, z } = ParseTemporalInstantString(RequireString(item));
   assert(time !== 'start-of-day', 'Instant string must include a time');
-  const {
-    hour = 0,
-    minute = 0,
-    second = 0,
-    millisecond = 0,
-    microsecond = 0,
-    nanosecond = 0
-  } = time;
+  const { hour = 0, minute = 0, second = 0, millisecond = 0, microsecond = 0, nanosecond = 0 } = time;
 
   // ParseTemporalInstantString ensures that either `z` is true or or `offset` is non-undefined
   const offsetNanoseconds = z ? 0 : ParseDateTimeUTCOffset(offset);
